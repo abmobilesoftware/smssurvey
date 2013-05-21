00e708a41f9f9918ede08a992893931d7f466bcd
Ando <dragos@txtfeedback.net>
2013-05-21 22:04:51 +0300

    Added pie chart on Questions detail view

--- smsSurvery.Surveryer/Controllers/ReportsController.cs
+++ smsSurvery.Surveryer/Controllers/ReportsController.cs
@@ -4,83 +4,111 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using smsSurvery.Surveryer.ReportHelpers;
 
 namespace smsSurvery.Surveryer.Controllers
 {
-    public class ReportsController : Controller
-    {
-       private smsSurveyEntities db = new smsSurveyEntities();
+   public class ReportsController : Controller
+   {
+      private const string cRatingsTypeQuestion = "Rating";
+      private const string cFreeTextTypeQuestion = "FreeText";
+      private smsSurveyEntities db = new smsSurveyEntities();
 
-       [HttpGet]
-       public ActionResult GetSurveyResults(int surveyId)
-       {
-          //for each question in the survey, aggregate the results
-          SurveyPlan survey = db.SurveyPlanSet.Find(surveyId);
-          if (survey != null)
-          {
-             foreach (var question in survey.Questions)
-             {
-                switch (question.Type)
-                {
-                   case "Rating":
-                      var res = GenerateResultForRatingQuestion(question);
-                      break;
-                   case "FreeText":
+      public const string STRING_COLUMN_TYPE = "string";
+      public const string NUMBER_COLUMN_TYPE = "number";
 
-                      break;
-                   default:
-                      break;
-                }
-             }
-          }
-          return null;
-       }
+      [HttpGet]
+      public JsonResult GetSurveyResults(int surveyId)
+      {
+         //for each question in the survey, aggregate the results
+         SurveyPlan survey = db.SurveyPlanSet.Find(surveyId);
+         QuestionSurveyResults res = null;
+         if (survey != null)
+         {
+            foreach (var question in survey.Questions)
+            {
+               switch (question.Type)
+               {
+                  case cRatingsTypeQuestion:
+                      res = GenerateResultForRatingQuestion(question);
+                     break;
+                  case cFreeTextTypeQuestion:
 
-       private QuestionSurveyResults GenerateResultForRatingQuestion(Question q)
-       {
-          //we need the possible values - only valid answers are to be considered
-          /**the output should contain
-           * Total number of answers & total number of valid answers
-           * For each valid value the number of answers
-           */
-          Dictionary<String, int> resultsPerAnswer = new Dictionary<String, int>();          
-          HashSet<string> validPossibleAnswers = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
-          foreach (string validAnswerValue in q.ValidAnswers.Split(';'))
-          {
-             resultsPerAnswer.Add(validAnswerValue, 0);
-             validPossibleAnswers.Add(validAnswerValue);
-          }
+                     break;
+                  default:
+                     break;
+               }
+            }
+         }
+         if (res != null)
+         {
+            var row1 = new RepDataRow(new RepDataRowCell[] { new RepDataRowCell("Incomming SMS", "Incomming"), new RepDataRowCell(13, "blabla") });
+            var row2 = new RepDataRow(new RepDataRowCell[] { new RepDataRowCell("Outgoing SMS", "Outgoing"), new RepDataRowCell(25, "somethings") });
+            var row3 = new RepDataRow(new RepDataRowCell[] { new RepDataRowCell("Outgoing SMS", "Outgoing"), new RepDataRowCell(11, "11") });
+            List<RepDataRow> pieChartContent = new List<RepDataRow>();
+            foreach (var rowData in res.AnswersPerValidOption)
+            {
+               var row = new RepDataRow(new RepDataRowCell[] { new RepDataRowCell(rowData.Key, rowData.Key), new RepDataRowCell(rowData.Value, rowData.Value.ToString()) });
+               pieChartContent.Add(row);
+            }
 
-          int validResponses = 0;
-          foreach (var result in q.Result)
-          {
-             if (validPossibleAnswers.Contains(result.Answer)) 
-             {
-                resultsPerAnswer[result.Answer] = resultsPerAnswer[result.Answer] + 1;
-                validResponses++;
-             }
-          }
-          //now in resultsPerAnswer we should have, per possible answer value the number of responses and in validResponses the total number of valid/acceptable responses
-          QuestionSurveyResults outcome = new QuestionSurveyResults();
-          outcome.TotalNumberOfAnswers = q.Result.Count();
-          outcome.TotalNumberOfValidAnswers = validResponses;
-          outcome.ValidOptions = validPossibleAnswers;
-          outcome.AnswersPerValidOption = resultsPerAnswer;
-          return outcome;
-       }
-       
-       protected override void Dispose(bool disposing)
-       {
-          db.Dispose();
-          base.Dispose(disposing);
-       }
+            RepChartData pieChartSource = new RepChartData(
+               new RepDataColumn[] {
+                new RepDataColumn("17", STRING_COLUMN_TYPE, "Type"),
+                new RepDataColumn("18", STRING_COLUMN_TYPE, "Value") },
+                  pieChartContent);
+            return Json(pieChartSource, JsonRequestBehavior.AllowGet);
+         }
+         return null;
+      }
 
-       public class QuestionSurveyResults
-       {
-          public int TotalNumberOfAnswers { get; set; }
-          public int TotalNumberOfValidAnswers { get; set; }
-          public HashSet<string> ValidOptions { get; set; }
-          public Dictionary<string, int> AnswersPerValidOption { get; set; }
-       }
-    }
+      private QuestionSurveyResults GenerateResultForRatingQuestion(Question q)
+      {
+         //we need the possible values - only valid answers are to be considered
+         /**the output should contain
+          * Total number of answers & total number of valid answers
+          * For each valid value the number of answers
+          */
+         Dictionary<String, int> resultsPerAnswer = new Dictionary<String, int>();
+         HashSet<string> validPossibleAnswers = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+         foreach (string validAnswerValue in q.ValidAnswers.Split(';'))
+         {
+            resultsPerAnswer.Add(validAnswerValue, 0);
+            validPossibleAnswers.Add(validAnswerValue);
+         }
+
+         int validResponses = 0;
+         foreach (var result in q.Result)
+         {
+            if (validPossibleAnswers.Contains(result.Answer))
+            {
+               resultsPerAnswer[result.Answer] = resultsPerAnswer[result.Answer] + 1;
+               validResponses++;
+            }
+         }
+         //now in resultsPerAnswer we should have, per possible answer value the number of responses and in validResponses the total number of valid/acceptable responses
+         QuestionSurveyResults outcome = new QuestionSurveyResults();
+         outcome.TotalNumberOfAnswers = q.Result.Count();
+         outcome.TotalNumberOfValidAnswers = validResponses;
+         outcome.ValidOptions = validPossibleAnswers;
+         outcome.AnswersPerValidOption = resultsPerAnswer;
+         return outcome;
+      }
+
+      protected override void Dispose(bool disposing)
+      {
+         db.Dispose();
+         base.Dispose(disposing);
+      }
+
+      public class QuestionSurveyResults
+      {
+         public int TotalNumberOfAnswers { get; set; }
+         public int TotalNumberOfValidAnswers { get; set; }
+         public HashSet<string> ValidOptions { get; set; }
+         public Dictionary<string, int> AnswersPerValidOption { get; set; }
+      }
+
+
+   }
 }
---
+++ smsSurvery.Surveryer/Helpers/ReportHelpers.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace smsSurvery.Surveryer.ReportHelpers 
+{
+   public class RepDataColumn
+   {
+      public string id;
+      public string label;
+      public string type;
+
+      public RepDataColumn(string iId, string iType, string iLabel = "")
+      {
+         id = iId;
+         type = iType;
+         label = iLabel;
+      }
+
+   }
+
+   public class RepDataRowCell
+   {
+      public object v;
+      public string f;
+
+      public RepDataRowCell(object iV, string iF = "")
+      {
+         v = iV;
+         f = iF;
+      }
+
+   }
+
+   public class RepDataRow
+   {
+      /* 
+          * the reason of using an IEnumerable structure and not a pair 
+          * is because the json generated is a general json which fits 
+          * a large number of charts, one of them with multiple dimensions. 
+          * Also this structure is intended primarly to be displayed in tables with many columns.
+       */
+      public IEnumerable<RepDataRowCell> c;
+
+      public RepDataRow(IEnumerable<RepDataRowCell> iC)
+      {
+         c = iC;
+      }
+
+   }
+
+   public class ChartValue
+   {
+      public int value { get; set; }
+      public string description { get; set; }
+      public bool changed { get; set; }
+
+      public ChartValue(int iValue, string iDescription, bool iChanged = false)
+      {
+         value = iValue;
+         description = iDescription;
+         changed = iChanged;
+      }
+   }
+
+   public class RepChartData
+   {
+      public IEnumerable<RepDataColumn> cols;
+      public IEnumerable<RepDataRow> rows;
+
+      public RepChartData(IEnumerable<RepDataColumn> iCols, IEnumerable<RepDataRow> iRows)
+      {
+         cols = iCols;
+         rows = iRows;
+      }
+
+   }    
+}
