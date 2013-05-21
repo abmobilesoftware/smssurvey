fde072e626e50d03788b32ef1365608f748cafc2
Ando <dragos@txtfeedback.net>
2013-05-21 10:46:31 +0300

    Added reports controller

---
+++ smsSurvery.Surveryer/Controllers/ReportsController.cs
@@ -0,0 +1,45 @@
+﻿using smsSurvey.dbInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace smsSurvery.Surveryer.Controllers
+{
+    public class ReportsController : Controller
+    {
+       private smsSurveyEntities db = new smsSurveyEntities();
+
+       public ActionResult GetSurveyResults(int surveyId)
+       {
+          //for each question in the survey, aggregate the results
+          SurveyPlan survey = db.SurveyPlanSet.Find(surveyId);
+          if (survey != null)
+          {
+             foreach (var question in survey.Questions)
+             {
+                switch (question.Type)
+                {
+                   case "Rating":
+                      
+                      break;
+                   case "FreeText":
+
+                      break;
+                   default:
+                      break;
+                }
+             }
+          }
+          return null;
+       }
+
+       
+       protected override void Dispose(bool disposing)
+       {
+          db.Dispose();
+          base.Dispose(disposing);
+       }
+    }
+}
