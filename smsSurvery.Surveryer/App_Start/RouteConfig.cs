ffe07191550651fa78a006499837e69927bf16b0
Ando <dragos@txtfeedback.net>
2013-08-13 18:35:25 +0300

    Added route for easier access Mobile Surveys

--- smsSurvery.Surveryer/App_Start/RouteConfig.cs
+++ smsSurvery.Surveryer/App_Start/RouteConfig.cs
@@ -12,7 +12,11 @@ namespace smsSurvery.Surveryer
       public static void RegisterRoutes(RouteCollection routes)
       {
          routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+         routes.MapRoute(
+                "MobileSurvey",                                           // Route name
+                "MobileSurvey/{id}",                            // URL with parameters
+                new { controller = "MobileSurvey", action = "Index" }  // Parameter defaults
+            );
          routes.MapRoute(
              name: "Default",
              url: "{controller}/{action}/{id}",
