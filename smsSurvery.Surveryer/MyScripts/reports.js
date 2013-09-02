688d360ce7cfd5aafdedbf4fc0a405eeb6fefbd7
Ando <dragos@txtfeedback.net>
2013-09-02 17:19:07 +0300

    Added loading icons to reports

--- smsSurvery.Surveryer/MyScripts/reports.js
+++ smsSurvery.Surveryer/MyScripts/reports.js
@@ -1,22 +1,39 @@
 ﻿window.app = window.app || {};
+window.report = window.report || {};
+window.report.repOverviewPiecharts = {};
+window.report.repPieCharts = {};
 window.app.piedata = {};
 window.app.yesnopiedata = {};
 window.app.overviewPieData = {};
 window.app.tags = [];
 window.app.displayReportsForRatingQ = function (questionId) {
-   var loadingIndicatorId = "#loadingIndicator" + questionId;
-   var reportChartID = "#pieChart_div" + questionId;
-   //var loadedReportChart = $(reportChartID)[0];
-   var piechart = new google.visualization.PieChart($(reportChartID)[0]);
-   google.visualization.events.addListener(piechart, 'select', function (e) {
-      var sel = piechart.getSelection();
-      var selectedValue = window.app.piedata[questionId].getValue(sel[0].row, 0);
-      var url = "/Answer/GetMessagesWithOnePredefinedAnswer?questionId=" + questionId + "&answer=" + selectedValue;
-      //window.location.href = url;
-      var win = window.open(url, "_blank");
-      win.focus();
-   });
-   //$(loadingIndicatorId).show();
+   var chartId = "#pieChart_div" + questionId;
+   var chartElem = $(chartId);
+   var loadingIndicatorId = "#loadingIndicator" + questionId;   
+   var loadingIndicator = $(loadingIndicatorId);
+
+   var pieChart = window.report.repPieCharts[questionId];
+   if (pieChart == undefined) {
+      pieChart = new google.visualization.PieChart($(chartId)[0]);
+      google.visualization.events.addListener(pieChart, 'select', function (e) {
+         var sel = pieChart.getSelection();
+         var selectedValue = window.app.piedata[questionId].getValue(sel[0].row, 0);
+         var url = "/Answer/GetMessagesWithOnePredefinedAnswer?questionId=" + questionId + "&answer=" + selectedValue;
+         var win = window.open(url, "_blank");
+         win.focus();
+      });
+   }
+   
+   var graphsContainer = $("#graphsContainer" + questionId);
+   var candidateHeight = graphsContainer.outerHeight();
+   candidateHeight = candidateHeight != 0 ? candidateHeight + "px" : "410px";
+   loadingIndicator.height(candidateHeight);
+  
+   graphsContainer.height(candidateHeight);
+   loadingIndicator.width(chartElem.outerWidth());
+   loadingIndicator.css("line-height", candidateHeight);
+   loadingIndicator.show();   
+   
    $.ajax({
       data: {
          questionId: questionId,
@@ -28,8 +45,7 @@ window.app.displayReportsForRatingQ = function (questionId) {
       url: "/Reports/GetSurveyQuestionResults",
       dataType: "json",
       async: true,
-      success: function (jsonData) {
-         //$(loadingIndicatorId).hide();
+      success: function (jsonData) {        
          var options = {
             backgroundColor: '#F5F8FA',
             sliceVisibilityThreshold: 0,
@@ -42,7 +58,7 @@ window.app.displayReportsForRatingQ = function (questionId) {
          };
          var piedata = new google.visualization.DataTable(jsonData.pie);
          window.app.piedata[questionId] = piedata;
-         piechart.draw(piedata, options);
+         pieChart.draw(piedata, options);
          
          var tablechart = new google.visualization.Table(document.getElementById('tableChart_div' + questionId));
          tabledata = new google.visualization.DataTable(jsonData.table);
@@ -51,7 +67,9 @@ window.app.displayReportsForRatingQ = function (questionId) {
             sliceVisibilityThreshold: 0,
             cssClassNames: { tableCell: "tCell" }
          };
-         tablechart.draw(tabledata, tableOptions);        
+         loadingIndicator.hide();
+         tablechart.draw(tabledata, tableOptions);
+
       }
    });
 };
@@ -76,6 +94,34 @@ window.app.displayReportsForFreeTextQ = function (questionId) {
 };
 
 window.app.displayReportOverview = function (surveyPlanId) {
+   //DA we should avoid recreating the chart as this is a costly operation
+   var chartId = "#pieOverviewChart_div" + surveyPlanId;
+   var chartElem = $(chartId);
+   var loadingIndicatorId = "#loadingOverviewIndicator" + surveyPlanId;
+   var loadingIndicator = $(loadingIndicatorId);
+   var pieChart = window.report.repOverviewPiecharts[surveyPlanId];
+   if (pieChart == undefined) {
+      pieChart = new google.visualization.PieChart(chartElem[0]);
+      google.visualization.events.addListener(pieChart, 'select', function (e) {
+         var sel = pieChart.getSelection();
+         var selectedValue = window.app.overviewPieData[surveyPlanId].getValue(sel[0].row, 0);
+         var url = "/Answer/GetCustomerWhichAnsweredXQuestions?surveyId=" + surveyPlanId + "&nrOfAnsweredQuestions=" + selectedValue;
+         var win = window.open(url, "_blank");
+         win.focus();
+      });
+   }
+   
+
+   var graphsContainer = $("#graphsOverviewContainer" + surveyPlanId);
+   var candidateHeight = graphsContainer.outerHeight();
+   candidateHeight = candidateHeight != 0 ? candidateHeight + "px" : "410px";
+   loadingIndicator.height(candidateHeight);
+
+   graphsContainer.height(candidateHeight);
+   loadingIndicator.width(chartElem.outerWidth());
+   loadingIndicator.css("line-height", candidateHeight);
+   loadingIndicator.show();   
+
    $.ajax({
       data: {
          surveyPlanId: surveyPlanId,
@@ -95,19 +141,12 @@ window.app.displayReportOverview = function (surveyPlanId) {
             'width': 'auto',
             'height': 300,
             'title': "Percentage of completion (out of 100)"
-         };
-         var piechart = new google.visualization.PieChart(document.getElementById('pieOverviewChart_div' + surveyPlanId));
+         };         
          var piedata = new google.visualization.DataTable(jsonData.pie);
-         window.app.overviewPieData[surveyPlanId] = piedata;
-         //window.app.piedata[questionId] = piedata;
-         piechart.draw(piedata, options);
-         google.visualization.events.addListener(piechart, 'select', function (e) {
-            var sel = piechart.getSelection();
-            var selectedValue = window.app.overviewPieData[surveyPlanId].getValue(sel[0].row, 0);
-            var url = "/Answer/GetCustomerWhichAnsweredXQuestions?surveyId=" + surveyPlanId + "&nrOfAnsweredQuestions=" + selectedValue;            
-            var win = window.open(url, "_blank");
-            win.focus();
-         });
+         window.app.overviewPieData[surveyPlanId] = piedata;         
+         loadingIndicator.hide();
+         pieChart.draw(piedata, options);
+        
          var tablechart = new google.visualization.Table(document.getElementById('tableOverviewChart_div' + surveyPlanId));
          tabledata = new google.visualization.DataTable(jsonData.table);
          var tableOptions = {
