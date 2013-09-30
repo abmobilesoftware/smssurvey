1a2d30ef04fa882a7d2b3fb10727b88a1ee1bd32
Mihai Batista <mihai@txtfeedback.net>
2013-09-30 18:26:46 +0300

    Restart survey after a specified interval

--- smsSurvery.Surveryer/MyScripts/SurveyUtilities.js
+++ smsSurvery.Surveryer/MyScripts/SurveyUtilities.js
@@ -42,3 +42,45 @@ SurveyUtilities.Utilities = (function () {
    }
    return innerClass;
 })();
+
+var Timer = (function () {
+   var innerClass = {};
+   var noOfMouseMoves = 0;
+   // 
+   var mouseDetectionInterval = 30000; // ms
+   var restartInterval = 3000;
+   var timer1;
+   var timer2;
+
+   innerClass.events = {
+      RESTART_SURVEY: "restartSurveyEvent"
+   };
+   innerClass.startTimer = function (filename) {
+      noOfMouseMoves = 0;
+      document.addEventListener("mousemove", innerClass.trackMouseMovement);
+      timer1 = setInterval(innerClass.checkIfUserIsActive, mouseDetectionInterval);
+   };
+   innerClass.stopTimer = function () {
+      clearInterval(timer1);
+   };
+   innerClass.trackMouseMovement = function () {
+      ++noOfMouseMoves;
+   };
+   innerClass.checkIfUserIsActive = function () {
+      var isUserAwayFromPage = noOfMouseMoves == 0 ? true : false;
+      if (isUserAwayFromPage) {
+         document.removeEventListener("mousemove", innerClass.trackMouseMovement);
+         innerClass.stopTimer();
+         innerClass.startSurvey();
+      } else {
+         noOfMouseMoves = 0;
+      }
+   };
+   innerClass.restartSurvey = function () {
+      setTimeout(innerClass.startSurvey, restartInterval);
+   };
+   innerClass.startSurvey = function () {
+      location.reload();
+   }
+   return innerClass;
+})();
