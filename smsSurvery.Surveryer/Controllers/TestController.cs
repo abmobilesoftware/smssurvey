d1eb230aa3c3b89d1d48df5fb81cb368270eee98
Mihai Batista <mihai@txtfeedback.net>
2013-10-28 11:01:24 +0200

    Cache images tests

---
+++ smsSurvery.Surveryer/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace smsSurvery.Surveryer.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CanvasTest()
+        {
+           return View();
+        }
+
+        public ActionResult BlobTest()
+        {
+           return View();
+        }
+    }
+}
