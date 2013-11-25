aff7732a0275dbe4cd74d64207f25a2c7d2900c4
Ando <dragos@txtfeedback.net>
2013-11-25 13:49:42 +0200

    Added sanitizer for select Many From Many (also added unit test)

---
+++ smsSurvery.Surveryer.Tests/Utility/SanitizerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using smsSurvery.Surveryer.Utilities;
+
+namespace smsSurvery.Surveryer.Tests.Utility
+{
+   [TestClass]
+   public class SanitizerTests
+   {
+
+      private TestContext testContextInstance;
+      public TestContext TestContext
+      {
+         get { return testContextInstance; }
+         set { testContextInstance = value; }
+      }
+
+      [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "SanitizerInput.csv", "SanitizerInput#csv", DataAccessMethod.Sequential)]
+      [DeploymentItem("d:\\Work\\SmsSurvey\\smsSurvery.Surveryer.Tests\\Utility\\SanitizerInput.csv")]
+      [TestMethod]
+      public void Sanitize_DifferentInputCombinations_ReturnsExpectedOutputs()
+      {
+         string input = TestContext.DataRow[0] as string;
+         string expectedOutput =  TestContext.DataRow[1] as string;
+         Assert.AreEqual(expectedOutput, Sanitizer.Sanitize(input));
+      }
+   }
+}
